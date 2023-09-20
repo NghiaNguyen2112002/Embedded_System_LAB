67535cf975b8afdac98b3e58f55d6fb2e82fac76
TrungNghia <nghianguyen2112002@gmail.com>
2023-09-20 15:06:38 +0700

    finish

--- ES_LAB/src/main.c
+++ ES_LAB/src/main.c
@@ -1,1 +1,24 @@
-void app_main() {}
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_log.h"
+
+#define  LAB1
+
+
+void app_main(void)
+{
+
+    
+
+    while(1){
+        printf("Hello world!\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    
+    esp_restart();
+}
