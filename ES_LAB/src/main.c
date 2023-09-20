cc09b0a7c9a3aad22063e5b60a28d8dc5231e28e
TrungNghia <nghianguyen2112002@gmail.com>
2023-09-20 16:05:12 +0700

    finished

--- ES_LAB/src/main.c
+++ ES_LAB/src/main.c
@@ -6,19 +6,79 @@
 #include "sdkconfig.h"
 #include "esp_log.h"
 
-#define  LAB1
+/************************************************
+*               LAB ID DEFINE                   * 
+*************************************************/
+#define     LAB2
+// #define     LAB1
+
+/************************************************
+*               VARIABLES DEFINE                * 
+*************************************************/
+TaskHandle_t PrintStuID_Handler;
+TaskHandle_t BTPolling_Handler;
 
 
+/************************************************
+*               FUNCTION DEFINE                 * 
+*************************************************/
+void PrintStuID(void* param);
+void BTPolling(void* param);
+
+
+
+/************************************************
+*               MAIN FUNCTION                   * 
+*************************************************/
 void app_main(void)
 {
 
+    printf("Begin\n");
     
+    xTaskCreate(PrintStuID, "PrintStuID", 2048, NULL, 4, &PrintStuID_Handler);
+    xTaskCreate(BTPolling, "BTPolling", 2048, NULL, 3, &BTPolling_Handler);
+
 
     while(1){
-        printf("Hello world!\n");
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
     }
 
     
     esp_restart();
+}
+
+
+
+void PrintStuID(void* param){
+    uint16_t i = 0;
+
+    while(1){
+        printf("%d.Nguyen Trung Nghia 2013875\n", i++);
+        // vTaskDelay(1000);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    vTaskDelete(NULL);
+
+}
+
+void BTPolling(void* param){
+    uint16_t i = 0;
+
+    gpio_config_t gpioConfig;
+    /* Config BUTTON pin */
+    gpioConfig.pin_bit_mask = (1 << 18);
+    gpioConfig.mode = GPIO_MODE_INPUT;
+    gpioConfig.pull_up_en = GPIO_PULLUP_ENABLE;
+    gpioConfig.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    gpioConfig.intr_type = GPIO_INTR_DISABLE;
+    gpio_config(&gpioConfig);
+
+    while(1){
+        while(gpio_get_level(18) != 0);
+        printf("%d.ESP32\n", i++);
+
+    }
+
+    vTaskDelete(NULL);
 }
